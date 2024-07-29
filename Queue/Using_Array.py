4180154ea9956b03349c0c9b9e42e8ff3d5b4ba1
sundaresanv2004 <sundaresanv2004@gmail.com>
2024-07-29 20:13:57 +0530

    Queue

--- Queue/Using_Array.py
+++ Queue/Using_Array.py
@@ -1,12 +1,52 @@
 class Queue:
-    def __init__(self):
+    def __init__(self, size):
+        self.size = size
         self.queue = []
     
     def enqueue(self, data):
-        self.queue.append(data)
+        if len(self.queue) < self.size:
+            self.queue.append(data)
+        else:
+            print("Queue is Full!")
     
-    def display(self):
-        if self.queue == []:
+    def dequeue(self):
+        if not self.queue:
             print("Queue is Empty!")
         else:
-            pass
+            print("Removed: ", self.queue.pop(0))
+    
+    def peek(self):
+        if not self.queue:
+            print("Queue is Empty!")
+        else:
+            print(self.queue[0])
+    
+    def isEmpty(self):
+        if not self.queue:
+            print("Queue is Empty!")
+        else:
+            print("Queue is not Empty")
+    
+    def isFull(self):
+        if  len(self.queue) == self.size:
+            print("Queue is Full!")
+        else:
+            print("Queue is not Full")
+    
+    def display(self):
+        if not self.queue:
+            print("Queue is Empty!")
+        else:
+            print(self.queue)
+
+Q = Queue(5)
+Q.isEmpty()
+Q.isFull()
+for i in range(1, 7):
+    Q.enqueue(i)
+Q.display()
+Q.isFull()
+Q.dequeue()
+Q.isEmpty()
+Q.peek()
+Q.display()
